4448e8be64e6d054f6fe03d283a325945c743697
Suhail P K <suhail.pk@epixelsolutions.com>
2024-10-23 11:34:08 +0530

    developed useref funtion for change the style of label in real dom

--- src/list/Label.js
+++ src/list/Label.js
@@ -1,10 +1,12 @@
-import React,{useContext} from "react";
+import React,{useContext, useRef, useState} from "react";
 import './Label.css'
 import { MyContext } from "./List";
 
 function Label(props){
 
     const label = useContext(MyContext)
+    const [toolTip, setToolTip] = useState(false)
+    const refObj = useRef()
 
     const style = props.isStock ? {background: 'green'} : {background: 'red'}
     const inventory = props.isStock ? "Stock In" : "Stock Out"
@@ -12,9 +14,27 @@ function Label(props){
         return null
     }
 
-    return <span onClick={() =>{
-        props.onAction(props.isStock ? 'stockin':'stockout')
-    }} className="label-section" style={style}>{inventory}</span>
+    const handleMouseEnter = (evt) =>{
+        console.log(refObj.current)
+
+        const width1 = evt.target.getBoundingClientRect().width;
+        const width2 = refObj.current.getBoundingClientRect().width;
+
+        refObj.current.style.left = `${-(width2 - width1) / 2}px`;
+
+        setToolTip(true)
+    }
+    const handleMouseLeave = () =>{
+        setToolTip(false)
+    }
+
+    return <><span onClick={() => {
+        props.onAction(props.isStock ? 'stockin' : 'stockout');
+    } } className="label-section" style={style} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+    >{inventory}</span>
+    <label ref={refObj} className={`tooltip ${toolTip ? 'show-tooltip':'hide-tooltip'}`}>
+        This is {props.isStock ? 'Stock In':'Stock Out'} tooltip
+    </label></>
 }
 
 
